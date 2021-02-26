99150be059edd51dc3b2cddfa9f60194484a3557
Rogelio Gonzalez <rogelio.gzz30@outlook.com>
2021-02-26 16:29:26 -0600

    Tarea 1 DWB

---
+++ TareaNorthWind/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using TareaNorthWind.DataAccess;
+
+namespace TareaNorthWind
+{
+    class Program
+    {
+        public static void Exercise()
+        {
+            NorthwindContext dataContext = new NorthwindContext();
+            var queryEmp = dataContext.Employees.Select(e => e);
+
+            var output = queryEmp.ToList();
+            Console.WriteLine(output);
+        }
+        static void Main(string[] args)
+        {
+            Exercise();
+        }
+    }
+}
